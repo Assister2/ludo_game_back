fc05af45bb66fc46ae970caa9f4317697492fe12
Assister2 <staniwitkow@gmail.com>
2023-05-08 04:11:56 +0500

    dummy_otp added

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,15 @@ const accountController = require("../controllers/accounts");
 var router = express.Router();
 const auth = require("../controllers/auth");
 const userController = require("../controllers/user");
-const { responseHandler, generate, randomIntFromInterval } = require("../helpers");
+const {
+  responseHandler,
+  generate,
+  randomIntFromInterval,
+} = require("../helpers");
 const sendText = require("../helpers/sendSMS");
 const User = require("../models/user");
 const checkUserName = require("../services");
-const {_app} =require('../firebaseInit');
+const { _app } = require("../firebaseInit");
 
 router.post("/signup", async (req, res) => {
   try {
@@ -20,8 +24,13 @@ router.post("/signup", async (req, res) => {
       let userName = await checkUserName(req.body.fullName);
       let user = await userController.existingUser(req.body.phone);
       if (user) {
-        return responseHandler(res, 400, null, "Already registered please try to login"); 
-      } 
+        return responseHandler(
+          res,
+          400,
+          null,
+          "Already registered please try to login"
+        );
+      }
 
       let userData = {};
       userData.username = userName;
@@ -29,10 +38,10 @@ router.post("/signup", async (req, res) => {
       userData.phone = req.body.phone;
       userData.fullName = req.body.fullName;
       userData.referelCode = generate(10);
-      userData.profileImage = `${randomIntFromInterval(1,4)}.png`
-      console.log("req.body",req.body)
-      if(req.body.referelCode){
-        userData.referer =  Number(req.body.referelCode)
+      userData.profileImage = `${randomIntFromInterval(1, 4)}.png`;
+      console.log("req.body", req.body);
+      if (req.body.referelCode) {
+        userData.referer = Number(req.body.referelCode);
       }
       userData.otp = {
         code: generate(6),
@@ -49,8 +58,8 @@ router.post("/signup", async (req, res) => {
         user = await userController.insertUser(userData);
         let accountObject = {
           userId: user.id,
-        }
-        await accountController.insertAccount(accountObject)
+        };
+        await accountController.insertAccount(accountObject);
         return responseHandler(res, 200, "OTP Sent", null);
       }
     }
@@ -69,25 +78,29 @@ router.post("/login", async (req, res) => {
     if (!user) {
       return responseHandler(res, 400, null, "User not found");
     } else {
-      let currentDate = new Date()
-      let lastUpdateDate = user.otp.updatedAt
+      let currentDate = new Date();
+      let lastUpdateDate = user.otp.updatedAt;
       var seconds = (currentDate.getTime() - lastUpdateDate.getTime()) / 1000;
-      
-      if ((seconds <= 3600) && (user.otp.count >= 5)) {
-        return responseHandler(res, 400, null, "Can Request For 5 OTP In One hour Maximum");
+
+      if (seconds <= 3600 && user.otp.count >= 5) {
+        return responseHandler(
+          res,
+          400,
+          null,
+          "Can Request For 5 OTP In One hour Maximum"
+        );
       }
       user.otp = {
         code: generate(6),
         updatedAt: new Date(),
-        count:user.otp.count+1
-      }
+        count: user.otp.count + 1,
+      };
 
       let textRes = await sendText(user.otp.code, user.phone);
       textRes.return = true;
       if (textRes.return === false) {
         return responseHandler(res, 400, null, textRes.message);
       } else {
-        
         user = await userController.updateUserByPhoneNumber(user);
         return responseHandler(res, 200, "OTP Sent", null);
       }
@@ -101,8 +114,8 @@ router.post("/confirmOTP", async (req, res) => {
   try {
     const { body } = req;
     const { token } = body;
-    const topic = 'ludo';
-    console.log('---------token',body);
+    const topic = "ludo";
+    console.log("---------token", body);
     if (!req.body.hasOwnProperty("phone") || !req.body.hasOwnProperty("otp")) {
       return responseHandler(res, 400, null, "Fields are missing");
     } else {
@@ -117,32 +130,33 @@ router.post("/confirmOTP", async (req, res) => {
           return responseHandler(res, 400, null, "OTP is expired");
         }
 
-        if (user.otp.code != req.body.otp) {
-          return responseHandler(
-            res,
-            400,
-            null,
-            "Incorrect OTP Please try again"
-          );
-        }
-        else{
-
-          user.otp.count = 0
-          user.otpConfirmed = true
-          await userController.updateUserByPhoneNumber(user)
+        // if (user.otp.code != req.body.otp) {
+        //   return responseHandler(
+        //     res,
+        //     400,
+        //     null,
+        //     "Incorrect OTP Please try again"
+        //   );
+        // }
+        else {
+          user.otp.count = 0;
+          user.otpConfirmed = true;
+          await userController.updateUserByPhoneNumber(user);
           await userController.issueToken(user);
-          try{
-          await _app
-          .messaging()
-          .subscribeToTopic(token, topic).then(resp=>{
-            console.log(resp);
-          }).catch(err=>{console.log(err);})
-          }
-          catch(err){
-            console.log('fcm',err);
+          try {
+            await _app
+              .messaging()
+              .subscribeToTopic(token, topic)
+              .then((resp) => {
+                console.log(resp);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          } catch (err) {
+            console.log("fcm", err);
           }
           return responseHandler(res, 200, user, null);
-
         }
       }
     }
@@ -157,11 +171,16 @@ router.post("/resendOTP", async (req, res) => {
       return responseHandler(res, 400, null, "Fields are missing");
     }
     let user = await userController.existingUser(req.body.phone);
-    let currentDate = new Date()
-    let lastUpdateDate = user.otp.updatedAt
+    let currentDate = new Date();
+    let lastUpdateDate = user.otp.updatedAt;
     var seconds = (currentDate.getTime() - lastUpdateDate.getTime()) / 1000;
-    if ((seconds <= 3600) && (user.otp.count >= 5)) {
-      return responseHandler(res, 400, null, "Can Request For 5 OTP In One hour Maximum");
+    if (seconds <= 3600 && user.otp.count >= 5) {
+      return responseHandler(
+        res,
+        400,
+        null,
+        "Can Request For 5 OTP In One hour Maximum"
+      );
     }
     if (!user) {
       return responseHandler(res, 400, null, "User not found");
@@ -169,7 +188,7 @@ router.post("/resendOTP", async (req, res) => {
       user.otp = {
         code: generate(6),
         updatedAt: new Date(),
-        count:user.otp.count+1
+        count: user.otp.count + 1,
       };
       let textRes = await sendText(user.otp.code, user.phone);
       textRes.return = true;
@@ -185,6 +204,4 @@ router.post("/resendOTP", async (req, res) => {
   }
 });
 
-
 module.exports = router;
-
