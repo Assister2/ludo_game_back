9eb4fdb8dabcb6204d9cf22293c255d377383866
Assister2 <staniwitkow@gmail.com>
2023-08-03 23:06:19 +0500

    live production _payment_fake started

--- controllers/transactionsControllers/routeHandlers.js
+++ controllers/transactionsControllers/routeHandlers.js
@@ -9,124 +9,126 @@ const {
 const getUPILink = require("./paymentUPI");
 const mongoose = require("mongoose");
 
-async function handleBuyChips(req, res) {
-  const session = await mongoose.startSession();
-
-  try {
-    session.startTransaction();
-    if (!req.body.payload) {
-      return responseHandler(res, 400, null, "Fields are missing");
-    }
-
-    const { amount } = req.body.payload;
-    const { user } = req;
-    if (amount <= 0 || amount > 20000) {
-      return responseHandler(res, 400, {}, "Amount limit is 0 to 20000");
-    }
-    const User = await userController.existingUserById({
-      id: user.id,
-    });
-    if (!User) {
-      return responseHandler(res, 400, {}, "User Not Found");
-    }
-    const transactionObject = {
-      amount: amount,
-      type: 0, //type 0 is for buying 1 for withdraw
-      status: 2, // 0 for failed 1 for success and 2 for pending
-      userId: user.id,
-    };
-    const Transaction = await transactionsController.insertNewTransaction(
-      transactionObject,
-      session
-    );
-    console.log("userByid", User);
-    const paymentUrl = await getUPILink(Transaction._id, amount, User);
-    const { status, data } = paymentUrl;
-    if (!status) {
-      throw new Error("payment failed");
-    }
-    await session.commitTransaction();
-    session.endSession();
-
-    return responseHandler(res, 200, data.payment_url, null);
-  } catch (error) {
-    if (session) {
-      await session.abortTransaction();
-      session.endSession();
-      return responseHandler(res, 400, {}, error);
-    }
-    console.log("BuyChipError", error);
-    throw error;
-  }
-}
-// for localhost
-
 // async function handleBuyChips(req, res) {
 //   const session = await mongoose.startSession();
+
 //   try {
+//     session.startTransaction();
 //     if (!req.body.payload) {
-//       return responseHandler(res, 400, null, "Fields are missing232");
+//       return responseHandler(res, 400, null, "Fields are missing");
 //     }
 
 //     const { amount } = req.body.payload;
 //     const { user } = req;
-//     const account = await accountController.getAccountByUserId(user.id);
-
-//     if (!account) {
-//       return responseHandler(res, 404, null, "Account not found");
-//     }
-
 //     if (amount <= 0 || amount > 20000) {
-//       return responseHandler(res, 400, account, "Amount limit is 0 to 20000");
+//       return responseHandler(res, 400, {}, "Amount limit is 0 to 20000");
 //     }
-
-//     let transactionId;
-//     let updatedAccount;
-
-//     await session.withTransaction(async () => {
-//       const transactionObject = {
-//         amount: amount,
-//         type: 0, //type 0 is for buying
-//         status: 1,
-//         userId: user.id,
-//       };
-
-//       const accountObject = {
-//         userId: user.id,
-//         depositCash: account.depositCash + amount,
-//         wallet: account.wallet + amount,
-//         withdrawRequest: false,
-//       };
-
-//       transactionId = await transactionsController.insertNewTransaction(
-//         transactionObject,
-//         session
-//       );
-
-//       updatedAccount = await accountController.updateAccountByUserId(
-//         accountObject,
-//         session
-//       );
-
-//       const history = new History();
-//       history.userId = user.id;
-//       history.historyText = "Chips Added Via UPI";
-//       history.createdAt = req.body.payload.createdAt;
-//       history.closingBalance = updatedAccount.wallet;
-//       history.amount = Number(amount);
-//       history.type = "buy";
-//       history.transactionId = transactionId._id;
-//       await history.save({ session });
+//     const User = await userController.existingUserById({
+//       id: user.id,
 //     });
-
-//     return responseHandler(res, 200, updatedAccount, null);
-//   } catch (error) {
-//     console.log("error", error);
-//     throw error;
-//   } finally {
+//     if (!User) {
+//       return responseHandler(res, 400, {}, "User Not Found");
+//     }
+//     const transactionObject = {
+//       amount: amount,
+//       type: 0, //type 0 is for buying 1 for withdraw
+//       status: 2, // 0 for failed 1 for success and 2 for pending
+//       userId: user.id,
+//     };
+//     const Transaction = await transactionsController.insertNewTransaction(
+//       transactionObject,
+//       session
+//     );
+//     console.log("userByid", User);
+//     const paymentUrl = await getUPILink(Transaction._id, amount, User);
+//     const { status, data } = paymentUrl;
+//     if (!status) {
+//       throw new Error("payment failed");
+//     }
+//     await session.commitTransaction();
 //     session.endSession();
+
+//     return responseHandler(res, 200, data.payment_url, null);
+//   } catch (error) {
+//     if (session) {
+//       await session.abortTransaction();
+//       session.endSession();
+//       return responseHandler(res, 400, {}, error);
+//     }
+//     console.log("BuyChipError", error);
+//     throw error;
 //   }
 // }
+
+
+// for localhost
+
+async function handleBuyChips(req, res) {
+  const session = await mongoose.startSession();
+  try {
+    if (!req.body.payload) {
+      return responseHandler(res, 400, null, "Fields are missing232");
+    }
+
+    const { amount } = req.body.payload;
+    const { user } = req;
+    const account = await accountController.getAccountByUserId(user.id);
+
+    if (!account) {
+      return responseHandler(res, 404, null, "Account not found");
+    }
+
+    if (amount <= 0 || amount > 20000) {
+      return responseHandler(res, 400, account, "Amount limit is 0 to 20000");
+    }
+
+    let transactionId;
+    let updatedAccount;
+
+    await session.withTransaction(async () => {
+      const transactionObject = {
+        amount: amount,
+        type: 0, //type 0 is for buying
+        status: 1,
+        userId: user.id,
+      };
+
+      const accountObject = {
+        userId: user.id,
+        depositCash: account.depositCash + amount,
+        wallet: account.wallet + amount,
+        withdrawRequest: false,
+      };
+
+      transactionId = await transactionsController.insertNewTransaction(
+        transactionObject,
+        session
+      );
+
+      updatedAccount = await accountController.updateAccountByUserId(
+        accountObject,
+        session
+      );
+
+      const history = new History();
+      history.userId = user.id;
+      history.historyText = "Chips Added Via UPI";
+      history.createdAt = req.body.payload.createdAt;
+      history.closingBalance = updatedAccount.wallet;
+      history.amount = Number(amount);
+      history.type = "buy";
+      history.transactionId = transactionId._id;
+      await history.save({ session });
+    });
+
+    return responseHandler(res, 200, updatedAccount, null);
+  } catch (error) {
+    console.log("error", error);
+    throw error;
+  } finally {
+    session.endSession();
+  }
+}
 
 async function handleSellChips(req, res) {
   const session = await mongoose.startSession();
