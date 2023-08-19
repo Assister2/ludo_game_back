bb2e0905eb399140b15c3976bf64e00ff1bf72ee
Assister2 <staniwitkow@gmail.com>
2023-08-20 02:20:50 +0500

    Revert "update session management"
    This reverts commit d36c3e8833574a4c71cbb0286ebdf2034df0f076.

--- database/db.js
+++ database/db.js
@@ -5,12 +5,10 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    console.log(config.DB_URI);
     await mongoose.connect(config.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
-      retryWrites: false
     });
     console.log("MongoDB connected");
   } catch (error) {
